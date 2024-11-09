cb558bcfc838f68c843676ec786d92e06dd07d41
pedro <pedrobtenorio@gmail.com>
2024-11-08 21:41:53 -0300

    floyd e dijkstra concluidos

--- README.md
+++ README.md
@@ -1,1 +1,55 @@
-# grafos
+# Teoria dos Grafos
+ Implementação dos algoritmos: 
+  * kosaraju
+  * Prim
+  * Kruskal
+  * Dijkstra
+  * Floyd-Warshall
+
+## Estrutura de Diretórios
+Cada algoritmo está em uma pasta separada, contendo seu próprio arquivo Makefile.
+
+## Compilação e Execução
+
+Para compilar e executar qualquer um dos algoritmos, siga as instruções abaixo:
+
+### Passo 1: Navegue até a pasta do algoritmo
+
+Use o comando abaixo para entrar na pasta do algoritmo que deseja compilar:
+
+    cd nome_da_pasta_do_algoritmo
+
+Exemplo para o algoritmo de Dijkstra:
+
+    cd dijkstra
+
+### Passo 2: Compile o código
+
+Use o `make` para compilar o código.
+
+### Passo 3: Execute o Algoritmo
+
+Após a compilação, execute o algoritmo com a opção `-h` para ver as instruções detalhadas de uso.
+
+    ./dijkstra.exe -h    # No Windows
+    ./dijkstra -h        # No Unix/Linux
+
+## Observações
+  * Todas as implementações descritas consideram grafos com vértices identificados por números inteiros maiores que 0.
+  * As saídas dos algoritmos não seguem um padrão específico.
+  * Todas as implementações descritas consideram arquivos de entrada no seguinte formato:
+
+      ```
+      |V| |E|
+      ```
+      seguido de |E| linhas na forma:
+      
+      ```
+      Vn Vm W(VnVm)
+      ```
+      Sendo: 
+      * |V| o número de vértices do grafo;
+      * |E| o número de arestas do grafo;
+      * Vn e Vm vértices distintos de V;
+      * VnVm uma aresta de E formada pelos vértices Vn e Vm;
+      * W(VnVm) o peso da aresta VnVm (para grafos sem pesos, esse elemento pode ser omitido no arquivo de entrada).
---
+++ floyd/floyd.cpp
@@ -0,0 +1,159 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <cmath>
+#include <limits>
+
+using namespace std;
+
+typedef vector<vector<double>> matrix;
+
+struct Graph {
+    matrix adjMatrix;
+    int numberOfNodes;
+};
+
+void printHelp() {
+    cout << "E necessario passar um arquivo de entrada para este programa, caso contrario ele nao sera executado." << endl;
+    cout << "Caso nao seja passado um arquivo de saida, sera criado um arquivo de saida de nome 'floyd-saida.txt'." << endl << endl;
+    cout << "Lista de comandos:" << endl << endl;
+    cout << "-h : mostra as informacoes sobre a execucao do programa." << endl;
+    cout << "-o <arquivo> : redireciona a saida para o 'arquivo'." << endl;
+    cout << "-f <arquivo> : indica o 'arquivo' que contem o grafo de entrada." << endl;
+    cout << "-s : mostra a solucao (em ordem crescente)." << endl;
+    exit(0);
+}
+
+vector<double> getEdgeValues(const string& fileLine) {
+    vector<double> edgeValues(3, 0);
+    stringstream tempStream(fileLine);
+    for(int j = 0; j < 3; j++) {
+        tempStream >> edgeValues[j];
+    }
+    return edgeValues;
+}
+
+void initializeGraphAdjMatrix(Graph& graph) {
+    matrix adjMatrix(graph.numberOfNodes + 1, vector<double>(graph.numberOfNodes + 1, numeric_limits<double>::infinity()));
+    graph.adjMatrix = adjMatrix;
+}
+
+void createGraphFromFile(Graph& newGraph, const string& inputFilePath) {
+    ifstream inputFile(inputFilePath);
+    if (!inputFile.is_open()) {
+        cerr << "Erro ao abrir o arquivo de entrada: " << inputFilePath << endl;
+        exit(1);
+    }
+
+    string fileLine;
+    int numberOfVertices, numberOfEdges;
+    getline(inputFile, fileLine);
+    stringstream(fileLine) >> numberOfVertices >> numberOfEdges;
+
+    newGraph.numberOfNodes = numberOfVertices;
+    initializeGraphAdjMatrix(newGraph);
+
+    vector<double> edgeValues;
+    for(int i = 0; i < numberOfEdges; i++) {
+        getline(inputFile, fileLine);
+        edgeValues = getEdgeValues(fileLine);
+        newGraph.adjMatrix[edgeValues[0]][edgeValues[1]] = edgeValues[2];
+    }
+
+    inputFile.close();
+}
+
+void initializeDistMatrix(const Graph& graph, matrix& distMatrix) {
+    distMatrix = graph.adjMatrix;
+}
+
+void initializePredecessorMatrix(const Graph& graph, vector<vector<int>>& predecessors) {
+    predecessors = vector<vector<int>>(graph.numberOfNodes + 1, vector<int>(graph.numberOfNodes + 1, 0));
+    for(int i = 1; i <= graph.numberOfNodes; i++) {
+        predecessors[i][i] = 0;
+    }
+}
+
+void printResult(int numberOfNodes, const matrix& distMatrix, const vector<vector<int>>& predecessors, bool print, ofstream* file) {
+    for(int i = 1; i <= numberOfNodes; i++) {
+        for(int j = 1; j <= numberOfNodes; j++) {
+            if(distMatrix[i][j] != numeric_limits<double>::infinity()) {
+                if(i != j) {
+                    *file << "O menor custo de " << i << " para " << j << " é " << distMatrix[i][j] << "." << endl;
+                    if(print) {
+                        cout << "O menor custo de " << i << " para " << j << " é " << distMatrix[i][j] << "." << endl;
+                    }
+                }
+            } 
+        }
+    }
+}
+
+void floyd(const Graph& graph, bool print, ofstream* file) {
+    matrix distMatrix;
+    vector<vector<int>> predecessors;
+
+    initializeDistMatrix(graph, distMatrix);
+    initializePredecessorMatrix(graph, predecessors);
+
+    for(int i = 1; i <= graph.numberOfNodes; i++) {
+        for(int j = 1; j <= graph.numberOfNodes; j++){
+            if(distMatrix[i][j] < numeric_limits<double>::infinity()) {
+                predecessors[i][j] = i;
+            }
+        }
+    }
+
+    for(int k = 1; k <= graph.numberOfNodes; k++) {
+        for(int i = 1; i <= graph.numberOfNodes; i++) {
+            for(int j = 1; j <= graph.numberOfNodes; j++) {
+                if(distMatrix[i][k] + distMatrix[k][j] < distMatrix[i][j]) {
+                    distMatrix[i][j] = distMatrix[i][k] + distMatrix[k][j];
+                    predecessors[i][j] = k;
+                }
+            }
+        }
+    }
+
+    printResult(graph.numberOfNodes, distMatrix, predecessors, print, file);
+}
+
+int main(int argc, char* argv[]) {
+    vector<string> args(argv, argv + argc);
+    string inputFilePath;
+    string outputFilePath = "floyd-saida.txt";
+    bool printOutput = false;
+    Graph graph;
+    ofstream outputFile;
+
+    for(int i = 0; i < argc; i++) {
+        if(args[i] == "-h") {
+            printHelp();
+        } else if(args[i] == "-o" && i < (argc - 1)) {
+            outputFilePath = args[i + 1];
+        } else if(args[i] == "-f" && i < (argc - 1)) {
+            inputFilePath = args[i + 1];
+        } else if(args[i] == "-s") {
+            printOutput = true;
+        }
+    }
+
+    if(inputFilePath.empty()) {
+        cerr << "Erro! E necessario passar um arquivo de entrada." << endl << endl;
+        printHelp();
+    }
+
+    createGraphFromFile(graph, inputFilePath);
+    outputFile.open(outputFilePath);
+    if (!outputFile.is_open()) {
+        cerr << "Erro ao abrir o arquivo de saida: " << outputFilePath << endl;
+        exit(1);
+    }
+
+    floyd(graph, printOutput, &outputFile);
+    outputFile.close();
+
+    return 0;
+}
